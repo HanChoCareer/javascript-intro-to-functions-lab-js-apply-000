f7dc3ad992ae048af8507bceb306906e6cf20237
Hankyol Cho <hancho.career@gmail.com>
2018-06-26 20:17:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,18 @@ function whisper(string){
 }
 
 function logShout(string){
-  console.log
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma (string){
+  if(string === string.toLowerCase())
+    return "I can't hear you!";
+  else if(string === string.toUpperCase())
+    return "YES INDEED!";
+  else
+    return "I love you, too.";
 }
